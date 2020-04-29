e27dc6885c059d0e3d12c3da41ec86c0a077b71b
KevinCastroP <castrok.c11@hotmail.com>
2020-04-29 14:03:18 -0500

    fixing pep8_2

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -17,6 +18,7 @@ app.register_blueprint(app_views)
 def teardown_db(response_or_exc):
     """Method to close the session"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST'),
